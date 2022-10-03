f5bb80541e5e35e03c041ba336d7b701d7a23281
Ksenia008 <114093379+Ksenia008@users.noreply.github.com>
2022-10-03 18:46:46 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "rus");
+	//1
+	/*int a;
+	cin >> a;
+	if (a == 0) {
+		cout << "верно" << endl;
+	}
+	else if (a != 0) {
+		cout << "неверно" << endl;
+	} */
+	//2	/*int a;	cin >> a;	if (a > 0) {		cout << "верно" << endl;	}	else if (a < 0) {		cout << "неверно" << endl;	}	system("pause");*/	//3	/*int a;	cin >>a;	if (a < 0) {		cout << "верно" << endl;	}	else {		cout << "неверно" << endl;	}*/	//4	/*int a;	cin>>a;	if (a >= 0) {		cout << "верно" << endl;	}	else if (a < 0) {		cout << "неверно" << endl;	}*/	//5	/*int a;	Cin>>a;	if (a <= 0) {		cout << "верно" << endl;	}	else if (a > 0) {		cout << "неверно" << endl;	}*/	//6	/*int a;	cin>>a;	if (a != 0) {		cout << "верно" << endl;	 }	else {		cout << "неверно" << endl;	}*/	//7	int a;	cin >> a;	if (a = "test") {		cout << "верно" << endl;	}	else {		cout << "неверно" << endl;	}		
+}
+
